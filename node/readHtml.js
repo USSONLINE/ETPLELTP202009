76f456251c1e66f376f67f4174619406e3676854
USS Uppuluri <uss@edvenswa.com>
2020-09-02 17:47:52 +0530

    Corrected HTML reading

--- node/readHtml.js
+++ node/readHtml.js
@@ -1,15 +1,8 @@
 var fs = require('fs');
-var http = require('http');
-var url = require('url');
 
 try {
-    //var data = fs.readFileSync('inputHtml.html', 'utf8');
-    //console.log(data);
-    http.createServer(function (req, res) {
-        var data = fs.readFileSync('inputHtml.html', 'utf8');
-        res.end(data);
-    }).listen(8778);
-    
+    var data = fs.readFileSync('inputHtml.html', 'utf8');
+    console.log(data);    
 } catch(e) {
     console.log('Error:', e.stack);
 }
