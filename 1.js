fbedea7cb3713df7268c41b67db468e10cb11115
Riyaz <47920712+Riyaz111@users.noreply.github.com>
2020-09-02 17:36:11 +0530

    Revert "initial changes"

--- 1.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-res.writeHead(200, {"Content-Type": "text/html"});
-res.end('welcome to edwensa');
-});
-server.listen(8081);
