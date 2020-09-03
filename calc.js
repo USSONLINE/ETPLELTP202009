4efc6d8317f6920e833dfb80bce85b76051860a8
USS Uppuluri <uss@edvenswa.com>
2020-09-03 10:45:38 +0530

    Enhanced by USS during the Trng

--- calc.js
+++ calc.js
@@ -1,9 +1,10 @@
+//Function to add two integers
 exports.add=function add(a,b){
     return a+b
 }
 
+//Function to subtract an integers from another
 exports.sub=function sub(a,b){
-
     return a-b
 }
 
