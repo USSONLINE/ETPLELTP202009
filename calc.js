5a3af39a94f1f7249a6dcf45056c0ed82858bc3c
sachin0605 <63196154+sachin0605@users.noreply.github.com>
2020-09-03 10:20:46 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,10 @@
+var cal = require('./cal.js') //importing a module
+
+result = cal.add(4, 5)
+console.log("the output is " + result);
+result = cal.sub(50, 25)
+console.log("the output is " + result);
+result = cal.mul(4, 5)
+console.log("the output is " + result);
+result = cal.div(50, 25)
+console.log("the output is " + result);
